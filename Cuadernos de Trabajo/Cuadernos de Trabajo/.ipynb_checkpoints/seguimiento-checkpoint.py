8ef377572a13836f02e9a3bc9a9649a031aba3a0
leninrkb <rkb.lenin@gmail.com>
2022-12-03 19:35:48 -0500

    termino modulo analisis de datos (por fin)

---
+++ Cuadernos de Trabajo/Cuadernos de Trabajo/.ipynb_checkpoints/seguimiento-checkpoint.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df_train = pd.read_csv('train.csv')
--- cursoEdube/ejercicio3.py
+++ cursoEdube/ejercicio3.py
@@ -1,11 +1,3 @@
-class Region():
-    dias=[]
-    region=''
-    def numeroDiasPrecipitaciones(self):
-        n = self.dias.count(0)
-        return n
-
-
 def dias(i):
     i+=1
     if i == 1: return 'lunes'
@@ -30,17 +22,65 @@ def ingresoRegion(region):
     r.dias = [int(input('dia '+dias(i)+': '))for i in range(7)]
     return r
 
-r=4
-reg=[ingresoRegion(regiones(region)) for region in range(r)]
-noPrecipitaciones=[]
+class Region():
+    def __init__(self):
+        self.dias=[]
+        self.region=''
+        self.porcentajes=[]
 
-for i in range(r):
-    if reg[i].numeroDiasPrecipitaciones() >= 2:
-        noPrecipitaciones.append(reg[i].region)
+    def numeroDiasPrecipitaciones(self):
+        n = self.dias.count(0)
+        return n
 
-print(f"""
-    Las regiones que no presentaron precipitaciones
-    al menos 2 días a la semana: {noPrecipitaciones}
-    El porcentaje de precipitaciones 
-    por cada día de la semana:
-""")
+    def totalPrecipitaciones(self):
+        t=0
+        for i in range(len(self.dias)):
+            t+=self.dias[i]
+        return t
+
+    def porcentajePrecipitacionesPorDia(self):
+        t = self.totalPrecipitaciones()
+        print('\nregion: '+self.region)
+        for i in range(len(self.dias)):
+            p = round((self.dias[i]*100/t),0)
+            self.porcentajes.append(p)
+            print('dia: '+dias(i),' porcentaje: '+str(p)+'%')
+
+    def dosDiasMenosLLuviosos(self):
+        print('\nlos dos dias menos lluviosos en la region '+self.region+' son')
+        self.dias.sort()
+        print('el dia '+dias(0)+' con '+str(self.dias[0]))
+        print('el dia '+dias(1)+' con '+str(self.dias[1]))
+
+class Reporte():
+    def __init__(self):
+        self.regiones=[]
+        self.nregiones=0
+
+    def mostrarPorcentajes(self):
+        for i in range(len(self.regiones)):
+            self.regiones[i].porcentajePrecipitacionesPorDia()
+
+    def ingreso(self):
+        r=4
+        for i in range(r):
+            self.regiones.append(ingresoRegion(regiones(i)))
+    def diasMenosLLuvia(self):
+        for i in range(len(self.regiones)):
+            self.regiones[i].dosDiasMenosLLuviosos()
+
+    def regionesNoPrecipitaciones(self):
+        noPrecipitaciones=[]
+        for i in range(self.nregiones):
+            if self.regiones[i].numeroDiasPrecipitaciones() >= 2:
+                noPrecipitaciones.append(reporte.regiones[i].region)
+        print(f"Las regiones que no presentaron precipitaciones al menos 2 días a la semana: {noPrecipitaciones}")
+
+
+reporte = Reporte()
+reporte.nregiones=4
+reporte.ingreso()
+
+reporte.regionesNoPrecipitaciones()
+reporte.mostrarPorcentajes()
+reporte.diasMenosLLuvia()
