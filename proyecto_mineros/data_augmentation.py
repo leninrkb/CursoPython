79246d1a8e57c4e86b235f8f2485e0fc698ff58a
leninrkb <rkb.lenin@gmail.com>
2023-01-25 15:00:01 -0500

    cambio modelo  ch

--- proyecto_mineros/data_augmentation.py
+++ proyecto_mineros/data_augmentation.py
@@ -6,27 +6,34 @@ import matplotlib.pyplot as plt
 transform = A.Compose([
     A.RandomRotate90(),
     A.Flip(),
+    A.HorizontalFlip(p=0.5),
+    A.RandomBrightnessContrast(p=0.4),
     A.Transpose(),
     A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45)
 ])
 
 
-PATH = 'D:/datasets/cuasapas_data/chaleco/nuevo_data_38'
-DIR = ['re']
-OUT_PATH='D:/datasets/cuasapas_data/chaleco/nuevo_data_38/aum'
+# PATH = 'D:/datasets/cuasapas_data/chaleco/nuevo_data_38'
+# DIR = ['re']
+# OUT_PATH='D:/datasets/cuasapas_data/chaleco/nuevo_data_38/aum'
+
+PATH = '/home/lenin/Documents/proyecto_mineros/datasets/nuevo_data_38'
+DIR = 'n'
+OUT_PATH='/home/lenin/Documents/proyecto_mineros/datasets/nuevo_data_38/aum_data/n'
+
+
 c2=0
 # for category in CATEGORIES:
 path=os.path.join(PATH, DIR)
 for img in os.listdir(path):
-    img_array=cv2.imread(os.path.join(pathimg))
+    img_array=cv2.imread(os.path.join(path, img))
     count=0
-    while(count<5):
+    while(count<2):
         augmented_image = transform(image=img_array)['image']
-        Datos='{}/img_{}_{}.jpg'.format(OUT_PATH,count,c2)
+        Datos='{}/aum_{}_{}.jpg'.format(OUT_PATH,count,c2)
         cv2.imwrite(Datos, augmented_image)
         count+=1
         c2+=1
-        
 print('total img =',c2)
         
 # count=0
--- proyecto_mineros/reconocer_chaleco.py
+++ proyecto_mineros/reconocer_chaleco.py
@@ -16,17 +16,14 @@ import cv2
 # no
 # chaleco = cv2.CascadeClassifier('modelos/mod_ch_15et_3.xml')
 
-chaleco = cv2.CascadeClassifier('modelos/cascade.xml')
-
-
-
+chaleco = cv2.CascadeClassifier('modelos/cascade_aumdata_13.xml')
 
 cap = cv2.VideoCapture(0)
 
 while (True):
     _, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    chalecos = chaleco.detectMultiScale(gray, scaleFactor = 7, minNeighbors = 20, minSize=(38,38))
+    chalecos = chaleco.detectMultiScale(gray, scaleFactor = 10, minNeighbors = 90, minSize=(38,38))
     for (x, y, w, h) in chalecos:
         cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
         cv2.putText(img,'chaleco',(x,y),2,0.7,(255,0,0),2,cv2.LINE_AA)
